3c9a46d944d7e06859eadffccd76b873a6f65aeb
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2022-05-23 07:58:28 +0200

    hinzufügen von kommentaren des groben programmablaufs

--- src/main.f90
+++ src/main.f90
@@ -1,5 +1,33 @@
 program haupt
   use kenngroessen
-  
+  ! -------------------------
+
+  ! Variablendeklarationen
+
+  ! Variableninitialisierungen
+
+  ! Einlesen der Datei
+
+  ! --- Pfad zur Datei
+
+  ! --- Datei verarbeiten
+
+  ! Deformationsmethode -- Aufbau der Globalen Steifigkeitsmatrix
+
+  ! Deformationsmethode -- Aufbau des Vektors der Knotenlasten
+
+  ! Deformationsmethode -- Aufbau des Vektors der Knotenverschiebungen
+
+  ! Deformationsmethode -- Lösen des Gleichungssystems
+  ! --- Auflagerknoten müssen in der Steifigkeitsmatrix
+
+  ! Deformationsmethode -- Rückwärtseinsetzen der Verschiebungen
+
+  ! Deformationsmethode -- Übertragen der ausgerechneten Schnittreaktionen in die lokalen KOS
+
+  ! Ausgeben der SKs und Auflagerreaktionen
+
+  ! Ausgeben mit Gnuplot
+
 
 end program haupt
