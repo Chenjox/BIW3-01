ce504d55bb34f61975f9803166c38f3334015fb9
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2022-05-23 09:05:01 +0200

    weiterbauen der einlese.f90

--- src/einlese.f90
+++ src/einlese.f90
@@ -1,9 +1,31 @@
 
-
+! ------------------------------------------------------------------------------
 subroutine einlesen(pfad, FehlerNummer)
+  ! ----------------------------------------------------------------------------
   use kenngroessen
   implicit none
+  ! Variablendeklarationen -----------------------------------------------------
   integer, INTENT(OUT) :: FehlerNummer
+  integer              :: ios, LaengeListe
   character(len=255), INTENT(IN) :: pfad
+  character(len=255)   :: momentaneZeile
+  real                 :: ZahlenListe(100)
+  ! Variableninitialisierungen -------------------------------------------------
+
+  ! Datei öffen
+
+  open(unit=20, file=pfad, status='old',iostat=ios)
+  if ( ios /= 0 ) stop "Error opening file 20"
+
+  read(unit=20,fmt='(A255)') momentaneZeile
+
+  call ffread(momentaneZeile, ZahlenListe, LaengeListe)
+
+  if(int(ZahlenListe(1)).lt.0) then
+    stop "Anzahl der Knoten ist kleiner als 0!"
+  end if
+  if(int(ZahlenListe(2)).lt.0) then
+    stop "Anzahl der Steabe ist kleiner als 0!"
+  end if
 
 end subroutine einlesen
