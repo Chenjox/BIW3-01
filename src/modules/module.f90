4bbb89b04ab6027e7862b52279be254a73da3348
ShavedLama <annonym@example.com>
2022-05-24 12:21:26 +0200

    Veränderung der Module Datei mit Ergänzung von Knoten

--- src/modules/module.f90
+++ src/modules/module.f90
@@ -3,9 +3,21 @@ module kenngroessen
 
   implicit none
 
-  type stab
+  int nk, ns                              !definieren von Knotenanzahl, Stabanzahl
+
+  !-----------------------------------------------------------------------------
+  !Deklarieren unserer Variablen für die Knotenanzahl und die Stabanzahl findet beim Einlesen
+
+  type FreierKnoten                       !definieren eines komplexen Datentypen Stab
     integer :: art
-    real*8 :: x(2),y(2),E,A,I
+    real :: x,y                           !Koordinaten des Knotens
+  end type
+
+  type(Knoten) :: Knoten(1:100)
+
+  type stab                               !definieren eines komplexen Datentypen Stab
+    integer :: art
+    real :: x(2),y(2),E,A,I               !x1,x2,y1,y2,E-Modul,QSFläche, Flächenträgheitsmoment
   end type stab
 
   type(stab) :: staebe(1:100)
