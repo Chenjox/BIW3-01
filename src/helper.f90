2be6e04cf1c4cd7c0cc9a99ba52e0470b855a5d6
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2022-06-09 15:26:13 +0200

    ADD: Untermatrizenalgorithmus

--- src/helper.f90
+++ src/helper.f90
@@ -33,3 +33,35 @@ subroutine writeMatrix(pfad, matrix, groesse)
 
   close(100)
 end subroutine writeMatrix
+
+function UnterMatrix(A,n,z,s)
+  implicit none
+  integer :: n,z,s ! groesse, zeile und spalte die weggelassen werden
+  real    :: A(n,n)
+  real   :: UnterMatrix(n-1,n-1)
+  integer :: i,j
+
+  zeilen: do i = 1, n
+    if ( i.eq.z ) then ! Die Zeile mit Index i wird ignoriert
+      cycle zeilen
+    end if
+    spalten: do j = 1, n
+      if ( j.eq.s ) then ! Die Spalte mit index s wird ignoriert
+        cycle spalten
+      else if ( j.gt.s ) then ! Ist j größer als s dann müssen wir immer eins von j abziehen
+        if(i.gt.z) then
+          UnterMatrix(i-1,j-1) = A(i,j) !wevon j>s und i>z von beiden 1 abziehen
+        else
+          UnterMatrix(i,j-1) = A(i,j) !wenn nur j>s nur von j (spalten) 1 abziehen
+        end if
+      else ! j ist kleiner als s
+        if(i.gt.z) then
+          UnterMatrix(i-1,j) = A(i,j) !wenn nur i>z nur von i (zeilen) 1 abziehen
+        else
+          UnterMatrix(i,j) = A(i,j)
+        end if ! wenn beides kleiner oder gleich nichts abziehen
+      end if
+    end do spalten
+  end do zeilen
+
+end function UnterMatrix
