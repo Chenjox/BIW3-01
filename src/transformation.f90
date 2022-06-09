ebfe16df59e5b246355814c4d41c015b0f3bcc6f
SirJowi <johann-sinner@web.de>
2022-06-09 14:54:01 +0200

    ADD: translokal in transformation

--- src/transformation.f90
+++ src/transformation.f90
@@ -30,3 +30,39 @@ real, dimension(3,3), INTENT(OUT):: matglobal
   call printMatrix(matglobal,3)
 
 endsubroutine
+
+
+!Transformation in lokale Koordinaten ------------------------------------------
+
+subroutine translokal(alpha,matglobal,matlokal)
+!globale Steifigkeitsmatrix in lokale Steifigkeitsmatrix umrechnen
+!Eingabegroessen: Transformationswinkel alpha, globale Steifigkeitsmatrix
+!Ausgabegroessen: lokale Steifigkeitsmatrix
+
+use kenngroessen
+
+implicit none
+
+real alpha
+real, dimension(3,3):: T
+real, dimension(3,3), INTENT(OUT):: matlokal
+real, dimension(3,3), INTENT(IN):: matglobal
+
+
+!Transformationsmatrix aus Transformationswinkel erstellen
+  T=0                                                     !Alle Zellen der Matrix zu Null
+
+  T(1,1) = cos(alpha)
+  T(1,2) = sin(alpha)
+  T(2,1) = -sin(alpha)
+  T(2,2) = cos(alpha)
+  T(3,3) = 1
+
+!Globale Steifigkeitsmatrix ausrechnen
+  matglobal = 0                                           !Funktionen von Fortran:
+  matglobal = MATMUL(TRANSPOSE(T),matglobal)              !MATMUL     - Matrizenmultiplikation
+                                                          !TRANSPOSE  - Transponierte Matrix erstellen
+
+  call printMatrix(matglobal,3)
+
+endsubroutine
